83cc199789a7de22d617f5a8dbcba97b5b622d50
skantaev <s.kantaev@gmail.com>
2018-06-21 00:58:09 +0300

    Some minor changes

--- my_store/settings.py
+++ my_store/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Media
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # Cart settings
